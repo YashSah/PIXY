c5720413cc88c05530d6684817ddef9d84c51517
YashSah <yashsahyashu4752@gmail.com>
2024-01-09 23:44:26 +0530

    TextFormField added to Chatbot feature

--- lib/screen/feature/chatbot_feature.dart
+++ lib/screen/feature/chatbot_feature.dart
@@ -14,6 +14,42 @@ class _ChatBotFeatureState extends State<ChatBotFeature> {
       appBar: AppBar(
         title: const Text("Chat with AI Assistant"),
       ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Expanded(child: TextFormField(
+              textAlign: TextAlign.center,
+              onTapOutside: (e) => FocusScope.of(context).unfocus(),
+              decoration: InputDecoration(
+                isDense: true,
+                hintText: "Ask me anything you want...",
+                hintStyle: const TextStyle(fontSize: 14),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+            )),
+
+            const SizedBox(width: 8,),
+
+            CircleAvatar(
+              radius: 24,
+              child: IconButton(
+                onPressed: () {},
+                icon: const  Icon(
+                  Icons.rocket_launch_rounded,
+                  color: Colors.white,
+                  size: 24,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+
       body: ListView(
         children: [
 
